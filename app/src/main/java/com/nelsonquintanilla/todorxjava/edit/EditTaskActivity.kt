3706c72ac85977719c34dbaabd4d23f9668d8d90
Nelson Quintanilla <nfquinta@gmail.com>
2024-07-19 13:29:16 +0800

    Refactor EditTaskActivity and EditTaskViewModel for improved readability and maintainability

--- app/src/main/java/com/nelsonquintanilla/todorxjava/edit/EditTaskActivity.kt
+++ app/src/main/java/com/nelsonquintanilla/todorxjava/edit/EditTaskActivity.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.Observer
 import com.jakewharton.rxbinding4.view.clicks
 import com.jakewharton.rxbinding4.widget.textChanges
 import com.nelsonquintanilla.todorxjava.database.TaskRoomDatabase
@@ -19,6 +18,7 @@ class EditTaskActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityEditTodoBinding
     private val disposables = CompositeDisposable()
+    private lateinit var viewModel: EditTaskViewModel
 
     companion object {
         private const val TASK_ID_KEY = "taskIdKey"
@@ -31,16 +31,32 @@ class EditTaskActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initializeView()
+        initializeViewModel()
+        setupObservers()
+        setupListeners()
+    }
+
+    private fun initializeView() {
         binding = ActivityEditTodoBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+    }
 
-        val viewModel = buildViewModel {
+    private fun initializeViewModel() {
+        viewModel = buildViewModel {
             val repository = RoomTaskRepository(TaskRoomDatabase.fetchDatabase(this))
             val taskIdKey = intent.getIntExtra(TASK_ID_KEY, RoomTaskRepository.INVALID_ID)
             EditTaskViewModel(repository, Schedulers.io(), taskIdKey)
         }
+    }
 
+    private fun setupObservers() {
+        viewModel.finishLiveData.observe(this) { finish() }
+        viewModel.textLiveData.observe(this) { binding.titleInput.append(it) }
+    }
+
+    private fun setupListeners() {
         // RxBinding method to listen for input event and forward it to the view model
         binding.done.clicks()
             .subscribe { viewModel.onFinishedClicked() }
@@ -50,9 +66,10 @@ class EditTaskActivity : AppCompatActivity() {
         binding.titleInput.textChanges()
             .subscribe { viewModel.onTextChanged(it) }
             .addTo(disposables)
+    }
 
-        viewModel.finishLiveData.observe(this, Observer { finish() })
-
-        viewModel.textLiveData.observe(this, Observer(binding.titleInput::append))
+    override fun onDestroy() {
+        super.onDestroy()
+        disposables.clear()
     }
 }
--- app/src/main/java/com/nelsonquintanilla/todorxjava/edit/EditTaskViewModel.kt
+++ app/src/main/java/com/nelsonquintanilla/todorxjava/edit/EditTaskViewModel.kt
@@ -4,7 +4,9 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.nelsonquintanilla.todorxjava.model.TaskItem
 import com.nelsonquintanilla.todorxjava.repository.TaskRepository
+import io.reactivex.rxjava3.core.Maybe
 import io.reactivex.rxjava3.core.Scheduler
+import io.reactivex.rxjava3.core.Single
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.kotlin.Observables
 import io.reactivex.rxjava3.kotlin.addTo
@@ -15,7 +17,7 @@ import java.util.Date
 class EditTaskViewModel(
     private val repository: TaskRepository,
     private val backgroundScheduler: Scheduler,
-    taskId: Int,
+    private val taskId: Int,
 ) : ViewModel() {
 
     private val disposables = CompositeDisposable()
@@ -26,17 +28,28 @@ class EditTaskViewModel(
     val textLiveData = MutableLiveData<String>()
 
     init {
+        initializeStreams()
+    }
+
+    private fun initializeStreams() {
         // Get a Maybe<TaskItem> representing the TaskItem being edited
         // If there's no TaskItem that corresponds to the passed in id, the Maybe will emit nothing
         // and complete. cache() is to use existing task in multiple places without remaking the
         // call every time (since that could be expensive)
         val existingTask = repository.getTask(taskId).cache()
 
+        initializeExistingTask(existingTask)
+        handleTaskSaving(existingTask)
+    }
+
+    private fun initializeExistingTask(existingTask: Maybe<TaskItem>) {
         existingTask
             .subscribeOn(backgroundScheduler)
             .subscribe { textLiveData.postValue(it.text) }
             .addTo(disposables)
+    }
 
+    private fun handleTaskSaving(existingTask: Maybe<TaskItem>) {
         // Combine both observables to have the latest text whenever the done button is tapped
         // and save TaskItem in the database
         Observables.combineLatest(finishedClicks, taskTitleTextChanges)
@@ -44,26 +57,29 @@ class EditTaskViewModel(
             // flatMapSingle() is used to convert from Observable to Single
             // Usually done when executing a network or database call that returns a Single after
             // some user interaction
-            .flatMapSingle { title ->
-                existingTask
-                    // Convert Maybe to Single.
-                    // If there's no TaskItem associated with the taskId passed into this view model,
-                    // we want to save a new TaskItem instead of modifying an existing one
-                    .defaultIfEmpty(
-                        TaskItem(null, title.toString(), Date(), false)
-                    )
-                    .flatMap {
-                        val taskItem = TaskItem(it.id, title.toString(), Date(), it.isDone)
-                        repository.insertTask(taskItem)
-                    }
-                    .subscribeOn(backgroundScheduler)
-            }
+            .flatMapSingle { title -> saveTask(existingTask, title) }
             .subscribe { finishLiveData.postValue(Unit) }
             .addTo(disposables)
     }
 
-    fun onFinishedClicked() = finishedClicks.onNext(Unit)
+    private fun saveTask(existingTask: Maybe<TaskItem>, title: CharSequence): Single<Long> {
+        return existingTask
+            // Convert Maybe to Single.
+            // If there's no TaskItem associated with the taskId passed into this view model,
+            // we want to save a new TaskItem instead of modifying an existing one
+            .defaultIfEmpty(createNewTask(title))
+            .flatMap {
+                val updatedTaskItem = it.copy(text = title.toString(), addedDate = Date())
+                repository.insertTask(updatedTaskItem)
+            }
+            .subscribeOn(backgroundScheduler)
+    }
 
+    private fun createNewTask(title: CharSequence): TaskItem {
+        return TaskItem(null, title.toString(), Date(), false)
+    }
+
+    fun onFinishedClicked() = finishedClicks.onNext(Unit)
     fun onTextChanged(text: CharSequence) = taskTitleTextChanges.onNext(text)
 
     override fun onCleared() {
